26ab49ba201cf244647040e0251425034c793e56
Lee Dong Woo <wlrnquf21@naver.com>
2020-03-20 21:56:27 +0900

    [Update] 0320 update
    player move & sprite image

---
+++ DodgeGame/Assets/Script/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float Speed;
+
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * Speed * Time.deltaTime, 0f, 0f));
+        }
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * Speed * Time.deltaTime, 0f));
+        }
+
+    }
+}
