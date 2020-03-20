584d60fa521e75a0bf2a3ae96c27d02ff80385a8
DongWoo <40193695+wlrnquf31@users.noreply.github.com>
2020-03-21 02:33:41 +0900

    [Update] 0321 update
    1. tile map
    2. orbit rotation shield

---
+++ DodgeGame/Assets/Script/ShieldOrbitRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldOrbitRotation : MonoBehaviour
+{
+
+    public GameObject Player;
+    public float Speed;
+
+    void Update()
+    {
+        orbitRotation();
+    }
+
+    private void orbitRotation()
+    {
+        transform.RotateAround(Player.transform.position, Vector3.forward, Speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+    }
+}
